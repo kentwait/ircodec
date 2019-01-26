7127965c372c3fbdda7a43a6ce00ddf75ff53826
Kent Kawashima <kentkawashima@gmail.com>
2019-01-26 13:30:02 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="ircodec",
+    version="0.0.1",
+    author="Kent Kawashima",
+    author_email="kentkawashima@gmail.com",
+    description="Send and receive IR commands using pigpio",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/kentwait/ircodec",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
